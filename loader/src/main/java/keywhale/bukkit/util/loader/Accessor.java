b2e739e077d0a7bc7139c14978c4d5058221c51e
keywhale <keywhale@github.com>
2026-03-20 09:44:01 -0400

    Change parameter order and add utility method

--- loader/src/main/java/keywhale/bukkit/util/loader/Accessor.java
+++ loader/src/main/java/keywhale/bukkit/util/loader/Accessor.java
@@ -1,19 +1,36 @@
 package keywhale.bukkit.util.loader;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
-public interface Accessor<VAL, ID> {
-    public void init(Access<VAL, ID> access);
+public interface Accessor<ID, VAL> {
+    public void init(Access<ID, VAL> access);
     public void cancel();
 
-    public static <VAL, ID> Accessor<VAL, ID> of(Function<Access<VAL, ID>, Runnable> fn) {
-        return new Accessor<VAL, ID>() {
+    public static <ID, VAL> Accessor<ID, VAL> of (Consumer<Access<ID, VAL>> consumer) {
+        return new Accessor<ID, VAL>() {
+
+            @Override
+            public void init(Access<ID, VAL> access) {
+                consumer.accept(access);
+            }
+
+            @Override
+            public void cancel() {
+                // Do nothing
+            }
+            
+        };
+    }
+
+    public static <ID, VAL> Accessor<ID, VAL> of(Function<Access<ID, VAL>, Runnable> function) {
+        return new Accessor<ID, VAL>() {
 
             private Runnable cancel;
 
             @Override
-            public void init(Access<VAL, ID> access) {
-                this.cancel = fn.apply(access);
+            public void init(Access<ID, VAL> access) {
+                this.cancel = function.apply(access);
             }
 
             @Override
